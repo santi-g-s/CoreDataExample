2cb83af5e809d6040f5040eca5de3826efeba4b5
Santiago Garcia Santos <santiago@garciasantos.org>
2022-07-09 10:43:01 -0400

    Initial Commit

---
+++ CoreDataExample/CoreDataExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataExampleApp.swift
+//  CoreDataExample
+//
+//  Created by Santiago Garcia Santos on 09/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
